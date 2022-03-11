03651422e09dd3da691f9a5a0b77e5204eaac398
Michigano <oguntugajoshua@gmail.com>
2022-03-11 05:16:37 -0800

    C program that prints exact

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - C program that prints exact sentence
- * Return: 0 if exited successfully
+ * main - C program thiat prints exact sentence
+ * Return: 1 if exited successfully
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
