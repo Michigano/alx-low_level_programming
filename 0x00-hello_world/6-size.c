66cf5a8ffaaffc94cbb83005cf82678c7799b634
Michigano <oguntugajoshua@gmail.com>
2022-03-10 20:13:37 -0800

    Size of tpes of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,12 @@
  */
 int main(void)
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
 	printf("Size of char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of long int: 4 byte(s)\nSize of long long int: 4 byte(s)\nSize of float: 4 byte(s)\n");
 	return (0);
 }
