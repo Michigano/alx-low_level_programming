86ea068528965cde1bba41ad06302e67dfa19d21
Michigano <oguntugajoshua@gmail.com>
2022-03-10 20:03:59 -0800

    Size of tpes of computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - printing sizes of various types of computer
+ * Return 0 if succesfull
+ */
+int main(void)
+{
+	printf("Size of char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of long int: 4 byte(s)\nSize of long long int: 4 byte(s)\nSize of float: 4 byte(s)\n");
+	return (0);
+}
