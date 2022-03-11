342ee2f72cf252686448fd8cd5af84a7b54f9fb7
Michigano <oguntugajoshua@gmail.com>
2022-03-10 19:31:32 -0800

    printing the exact grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+/**
+ * main - printing the proper grammar
+ * Return: 0 if exited succesfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n"0);
+	return (0);
+}
