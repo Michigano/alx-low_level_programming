0de8ca01dae811c32a311ea9e0bcafd018f8499d
Michigano <oguntugajoshua@gmail.com>
2022-03-12 13:50:17 -0800

    Combination of to gigits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/** main - Executing code for printing numbers with double digits
+ * Return: 0 if exited succesffuly
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a='0';n a <= '9'; a++)
+	{
+		for (b = '0'; b<= '9'; b++)
+		{
+			if (c < a)
+			{
+				putchar(a);
+				putchar(i);
+				
+				if (a != '8' || (a == '8' && i !=  '9' ))
+				
+					putchar(',');
+					putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
+}
