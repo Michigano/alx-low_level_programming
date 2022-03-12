6a0da0294f0315b1f5e7a0c0e4aedf9309662d4a
Michigano <oguntugajoshua@gmail.com>
2022-03-11 20:40:36 -0800

    Alphabet insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starting point
+ * Return: 0 is exited successfully
+ */
+int main(void)
+{
+	char lcase;
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+		putchar(lcase);
+		putchar('\n');
+	return (0);
+}
