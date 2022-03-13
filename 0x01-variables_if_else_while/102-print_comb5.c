813ef37f8c1d9cec13edb815afd41c14475eb336
Michigano <oguntugajoshua@gmail.com>
2022-03-13 15:01:10 -0700

    Write a program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing all possible combinations of two two-digit numbers.
+ * Return 0: if exited successfully
+ */
+int main(void)
+{
+	int t, d;
+
+	for (t = '0'; t <= '9'; t++)
+	{
+		for (d = t + 1; d <= '9'; d++)
+		{
+			putchar((t / 10) + '0');
+			putchar((t % 10) + '0');
+			putchar(' ');
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+			if (t == 98 && d == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
