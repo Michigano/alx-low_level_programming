db4effb8927315edd924af8b411c4ad66d211629
Michigano <oguntugajoshua@gmail.com>
2022-03-15 17:16:55 -0700

    Program that prints _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
  * The _putchar writes the character _putchar to stdout
  * @c: is the character to print
  *
- * Return: 1 if successful
+ * Return: on success 1
  * on error, -1 is returned and error is appropriately set
  */
 int _putchar(char c)
