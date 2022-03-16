f8eb948faf541c8086a126752dbb7ecc63458821
Michigano <oguntugajoshua@gmail.com>
2022-03-15 19:45:39 -0700

    Prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/* print_to_98 - prints all natural numbers from n to 98
+/**
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: Integer to be printed
  * Return: 0
  */
