1df877c856d54d7b227b3d7abb08a908a9c3edcf
Michigano <oguntugajoshua@gmail.com>
2022-03-15 18:27:55 -0700

    check for alphabetic characters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * Prints alphabet x10 - This will print in lowercase ten times
+ * print_alphabet_x10 - This will print in lowercase ten times
  * Return: void
  */
 void print_alphabet_x10(void)
