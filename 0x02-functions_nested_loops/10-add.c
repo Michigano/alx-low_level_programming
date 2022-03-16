319038662ed704252dcc1fd05932bfc8b5664cf3
Michigano <oguntugajoshua@gmail.com>
2022-03-15 19:24:54 -0700

    adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main"
+/**
+ * add - returns the sum of the parameters entered
+ * @a: an integer type one
+ * @b: an integer type two
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return ( a + b);
+}
