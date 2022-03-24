67987d0af878e0c65358f42f3b1fa22cb478cc23
Michigano <oguntugajoshua@gmail.com>
2022-03-24 06:44:37 -0700

    Write a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
+int check_seperators(char c);
 char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
